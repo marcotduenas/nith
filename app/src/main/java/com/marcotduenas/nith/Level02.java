1ad075c866f6769c0c82c3d74c369ed6231a24dd
Marco Tulio Duenas <marcot.dns@gmail.com>
2023-12-18 12:41:54 -0300

    Creating a new level for the game

--- app/src/main/java/com/marcotduenas/nith/Level02.java
+++ app/src/main/java/com/marcotduenas/nith/Level02.java
@@ -1,6 +1,9 @@
 package com.marcotduenas.nith;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,20 @@ public class Level02 extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.level_02);
+
+        NavController navControllerLevel02 = new NavController();
+        AnswersConfig levelChecker = new AnswersConfig(this);
+        EditText userInputtedAnswer = findViewById(R.id.inputCode);
+        Button enterCodeButton = findViewById(R.id.codeButton);
+
+        enterCodeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String literalUserAnswerValuer = userInputtedAnswer.getText().toString();
+                navControllerLevel02.saveUserAnswer(literalUserAnswerValuer);
+                levelChecker.correctAnswersList(navControllerLevel02.getUserAnswer());
+            }
+
+        });
     }
 }
