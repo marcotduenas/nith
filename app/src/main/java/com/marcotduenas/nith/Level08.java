7db948248fc0bbd9e0affb5e567d709bfa3bfa21
Marco Tulio <marcot.dns@gmail.com>
2023-12-23 05:26:07 -0300

    Adding levels 07 & 08

---
+++ app/src/main/java/com/marcotduenas/nith/Level08.java
@@ -0,0 +1,32 @@
+package com.marcotduenas.nith;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Level08 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.level_08);
+
+
+        AnswersConfig levelChecker = new AnswersConfig(this);
+        EditText userInputtedAnswer = findViewById(R.id.inputCode);
+        Button enterCodeButton = findViewById(R.id.codeButton);
+
+        enterCodeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String literalUserAnswerValue = userInputtedAnswer.getText().toString();
+                levelChecker.correctAnswersList(literalUserAnswerValue);
+            }
+
+        });
+    }
+}
