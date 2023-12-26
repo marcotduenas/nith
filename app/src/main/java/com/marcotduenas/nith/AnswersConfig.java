9746cc7cd23e95f9c50d18ea959645e17462d635
Marco Tulio <marcot.dns@gmail.com>
2023-12-26 17:22:43 -0300

    Remove .gitignore that was not pushing Answerconfig and addin the level 09
    I've chosen to remove the .gitignore because it was only being used to hide the game answers

---
+++ app/src/main/java/com/marcotduenas/nith/AnswersConfig.java
@@ -0,0 +1,38 @@
+package com.marcotduenas.nith;
+
+import android.content.Context;
+import android.content.Intent;
+import java.util.HashMap;
+
+public class AnswersConfig {
+
+    private Context context;
+    private HashMap <String, Class<?>> nextLevelClass;
+
+    public AnswersConfig(Context context){
+       this.context = context;
+    }
+
+    public void correctAnswersList(String sentAnswer){
+        nextLevelClass = new HashMap<>();
+        nextLevelClass.put("yes", Level02.class);
+        nextLevelClass.put("alkalinebatteries", Level03.class);
+        nextLevelClass.put("sertillanges", Level04.class);
+        nextLevelClass.put("animal", Level05.class);
+        nextLevelClass.put("1984", Level06.class);
+        nextLevelClass.put("superman", Level07.class);
+        nextLevelClass.put("passau", Level08.class);
+        nextLevelClass.put("salvador", Level09.class);
+        nextLevelClass.put("chainsaw", MainActivity.class);
+
+        Class isThereNextLevel = nextLevelClass.get(sentAnswer);
+
+        if (isThereNextLevel != null){
+            Intent intent = new Intent(context, isThereNextLevel);
+            context.startActivity(intent);
+        }
+
+
+    }
+
+}
